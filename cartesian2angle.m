ab59dd66c82c2839578f92615957924bf31de688
RatherBadCoder <69511402+RatherBadCoder@users.noreply.github.com>
2020-08-25 21:23:15 +1000

    Add files via upload

--- cartesian2angle.m
+++ cartesian2angle.m
@@ -1,29 +1,51 @@
 %Given XYZ (cartesian) coordinates, convert these to joint angles 
 close all; clear variables; clc;
-
-%Cartesian coordinates
+%% Cartesian coordinates
 goalPos = [-0.25,0.5,0.7+0.09465]; %X,Y,Z
 
+%% SETUP/CONNECT TO GAZEBO/ROS
+ipaddress = '192.168.0.18';
+robotType = 'Gazebo';
+rosshutdown;
+rosinit(ipaddress);
+blockposes = rossubscriber('/gazebo/link_states');
+pause(2);
+posdata = receive(blockposes,10);
+imSub = rossubscriber('/camera/color/image_raw');
+pcSub = rossubscriber('/camera/depth/points');
+testIm  = readImage(imSub.LatestMessage);
+imshow(testIm);
+rostopic list;
+
+%% Load robot and read joint states from Gazebo/ROS
 robot = loadrobot('universalUR5');
 showdetails(robot);
-%randPos = randomConfiguration(robot); %This is in angles, need to put in xyz! Is it even needed?
 
-%Forward kinematics
+%% Read Joint states
+jointSub = rossubscriber('joint_states'); %Needs to connect to ROS/Gazebo?
+JointStateGazebo = receive(jointSub);
+JointPositionGazebo = exampleHelperJointMsgToStruct(robot,JointStateGazebo);
+
+%% Forward kinematics
 wp1 = trvec2tform(goalPos); %Converts XYZ to a transform, produces similar result to fkine matrix
 disp('XYZ to T matrix');
 disp(wp1);
 
-%Inverse kinematics
+%% Inverse kinematics
 ik = inverseKinematics('RigidBodyTree',robot);
 weights = [0.25 0.25 0.25 1 1 1];
-initialguess = robot.homeConfiguration;
-[configSol,solInfo] = ik('tool0',wp1,weights,initialguess); %Feed i current joint state read from ROS instead of the initialguess
-
+%initialguess = robot.homeConfiguration;
+[configSol,solInfo] = ik('tool0',wp1,weights,JointPositionGazebo); %Feed in current joint state read from ROS
 %The end effector points upwards, multiply by -1 to flip it
 configSol(5).JointPosition = configSol(5).JointPosition*-1;
 
+%% Display
 figure(1);
 show(robot,configSol);
 title('From wp1 (trvec2tform)');
+
+for i = 1:1:numel(configSol)
+    disp(configSol(i).JointPosition);
+end
 
 %IS THIS LITERALLY JUST THE KINEMATICS THAT AGGY WROTE UP?!
